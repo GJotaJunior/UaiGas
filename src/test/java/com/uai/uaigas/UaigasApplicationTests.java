7929f4c9218e63540f23f72afb48b3a435118129
Gledson Junior <49466350+GJotaJunior@users.noreply.github.com>
2020-12-17 20:16:50 -0300

    Criação do projeto

---
+++ src/test/java/com/uai/uaigas/UaigasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uai.uaigas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaigasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
