72181611eba24f7da6e323ebea5949ae72cd9ee4
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2020-12-17 22:47:35 -0300

    CombustivelResource EnderecoResource tipoCombustivelRepository

---
+++ src/main/java/com/uai/uaigas/resources/TipoCombustivelResource.java
@@ -0,0 +1,59 @@
+package com.uai.uaigas.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.uai.uaigas.dto.TipoCombustivelDTO;
+import com.uai.uaigas.services.TipoCombustivelService;
+
+@RestController
+@RequestMapping(value = "/tipo-combustivel")
+public class TipoCombustivelResource {
+	
+	@Autowired
+	private TipoCombustivelService service;
+
+	@GetMapping
+	public ResponseEntity<List<TipoCombustivelDTO>> findAll() {	
+		List<TipoCombustivelDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<TipoCombustivelDTO> findById(@PathVariable Long id) {
+		TipoCombustivelDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TipoCombustivelDTO> insert(@RequestBody TipoCombustivelDTO dto) {
+		TipoCombustivelDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<TipoCombustivelDTO> update(@PathVariable Long id, @RequestBody TipoCombustivelDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+}
