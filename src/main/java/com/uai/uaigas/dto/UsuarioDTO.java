38282755b6940fcc18b3c2d0a38b1e189cd48446
Gledson Junior <49466350+GJotaJunior@users.noreply.github.com>
2020-12-17 21:40:45 -0300

    Criação UsuarioDTO

---
+++ src/main/java/com/uai/uaigas/dto/UsuarioDTO.java
@@ -0,0 +1,32 @@
+package com.uai.uaigas.dto;
+
+import java.io.Serializable;
+
+import com.uai.uaigas.entities.Usuario;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UsuarioDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String fotoUrl;
+    private boolean admin;
+
+    public UsuarioDTO(Usuario usuario) {
+	this.id = usuario.getId();
+	this.nome = usuario.getNome();
+	this.email = usuario.getEmail();
+	this.fotoUrl = usuario.getFotoUrl();
+	this.admin = usuario.isAdmin();
+    }
+
+    public Usuario toEntity() {
+	return Usuario.builder().id(id).nome(nome).email(email).fotoUrl(fotoUrl).admin(admin).build();
+    }
+}
