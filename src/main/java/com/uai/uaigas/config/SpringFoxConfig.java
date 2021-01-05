8104b50aeaae9efcf14c9c6d36fbc0c11e271adc
Gledson Junior <49466350+GJotaJunior@users.noreply.github.com>
2021-01-05 10:16:38 -0300

    Habilitado Swagger2 com validações do Bean

--- src/main/java/com/uai/uaigas/config/SpringFoxConfig.java
+++ src/main/java/com/uai/uaigas/config/SpringFoxConfig.java
@@ -2,13 +2,17 @@ package com.uai.uaigas.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
+@Import({springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class})
 public class SpringFoxConfig {                                    
     @Bean
     public Docket api() { 
